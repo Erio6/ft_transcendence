816d8d1f70fc48b2a34e6faee67b60e9d088c7c7
Erio6 <eliotgruscha@gmail.com>
2024-11-26 15:27:56 +0100

    add API, authentication, game djangoApp

--- backend/djangoProject/settings.py
+++ backend/djangoProject/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'API.apps.ApiConfig',
+    'authentication.apps.AuthenticationConfig',
+    'game.apps.GameConfig'
 ]
 
 MIDDLEWARE = [
@@ -106,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Luxembourg'
 
 USE_I18N = True
 
