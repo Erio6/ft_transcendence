997923489a55080217f1b8cd081e8e211f0f6ca7
El Mehdi Faiz <mehdifaiz42@outlook.com>
2025-02-04 15:46:33 +0100

    backup

--- src/backend/djangoProject/settings.py
+++ src/backend/djangoProject/settings.py
@@ -131,30 +131,30 @@ WSGI_APPLICATION = 'djangoProject.wsgi.application'
 
 #if you want to use docker uncommnet DATABASE and db_credentials
 
-db_credentials = get_db_credentials()
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": db_credentials['db_name'],
-        "USER": db_credentials['username'],
-        "PASSWORD": db_credentials['password'],
-        "HOST": db_credentials['host'],
-        "PORT": db_credentials['port'],
-    }
-}
-
+# db_credentials = get_db_credentials()
 
 # DATABASES = {
 #     "default": {
 #         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": "db",
-#         "USER": "postgres",
-#         "PASSWORD": "1234",
-#         "HOST": "127.0.0.1",
-#         "PORT": "5432",
+#         "NAME": db_credentials['db_name'],
+#         "USER": db_credentials['username'],
+#         "PASSWORD": db_credentials['password'],
+#         "HOST": db_credentials['host'],
+#         "PORT": db_credentials['port'],
 #     }
 # }
+
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "db",
+        "USER": "postgres",
+        "PASSWORD": "1234",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
+    }
+}
 
 # JWT
 
