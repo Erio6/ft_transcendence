665c8caf77b146a0f117801fac608d260d4d361b
Erio6 <eliotgruscha@gmail.com>
2025-02-05 16:37:11 +0100

    handle single player in game

--- src/backend/game/online_room.py
+++ src/backend/game/online_room.py
@@ -1,5 +1,7 @@
 import asyncio
 import json
+import threading
+import time
 
 from asgiref.sync import sync_to_async
 from django.shortcuts import redirect
@@ -84,7 +86,7 @@ class OnlineRoom(Room):
         if not self.is_tournament:
             asyncio.create_task(self.blockchain_recording_task(game.id))
 
-    async def blockchain_recording_task(self,game_id):
+    async def blockchain_recording_task(self, game_id):
         tx_hash = await blockchain_score_storage(game_id)
         game = await sync_to_async(Game.objects.get)(pk=game_id)
         if tx_hash:
@@ -131,6 +133,36 @@ class OnlineRoom(Room):
             await consumer.send(json.dumps({'type': 'redirect', 'url': "/"}))
             await consumer.close()
 
+    async def check_alone_async(self):
+        time.sleep(3)
+        if self.left_paddle and self.right_paddle:
+            print("Game Full")
+        elif self.left_paddle or self.right_paddle:
+            winner = self.left_paddle if self.left_paddle else self.right_paddle
+            game = await sync_to_async(Game.objects.get)(pk=self.id)
+            player_one = await sync_to_async(lambda: game.player_one)()
+            player_two = await sync_to_async(lambda: game.player_two)()
+            game.winner = winner.consumer.user_profile
+            game.winner_score = 10
+            game.looser_score = 0
+            game.player_one_score = 10 if winner == player_one else 0
+            game.player_two_score = 10 if winner == player_two else 0
+            game.end_time = now()
+            game.is_completed = True
+            await sync_to_async(game.save)(force_update=True)
+            tournament_game = await sync_to_async(TournamentGame.objects.get)(game=self.id)
+            tournament_id = await sync_to_async(lambda: tournament_game.tournament.id)()
+            new_url = "/tournament/tree/" + str(tournament_id) + "/"
+            if self.left_paddle:
+                await self.left_paddle.consumer.send(json.dumps({'type': 'redirect', 'url': new_url}))
+                await self.left_paddle.consumer.close()
+            if self.right_paddle:
+                await self.right_paddle.consumer.send(json.dumps({'type': 'redirect', 'url': new_url}))
+                await self.right_paddle.consumer.close()
+
+    def check_alone(self):
+        asyncio.run(self.check_alone_async())
+
     async def handle_paddle_msg(self, consumer, message):
         if self.left_paddle and consumer == self.left_paddle.consumer:
             if message['data'] == 1:
@@ -150,6 +182,8 @@ class OnlineRoom(Room):
     async def register_consumer(self, consumer):
         # if consumer == self.left_paddle.consumer or consumer == self.right_paddle.consumer or consumer in self.spectators:
         #     return False
+        if self.is_tournament and not self.left_paddle and not self.right_paddle:
+            threading.Thread(target=self.check_alone, daemon=True).start()
         if not self.left_paddle:
             self.left_paddle = Paddle("left", consumer, consumer.user_profile.display_name)
             await consumer.send(json.dumps({'type': 'client', 'loc': 'left', 'speed': self.left_paddle.speed}))
