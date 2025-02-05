c2afecd1f592ea5987634e7ff5efc4555897be80
Erio6 <eliotgruscha@gmail.com>
2025-02-05 11:19:02 +0100

    update css

--- src/backend/user/forms.py
+++ src/backend/user/forms.py
@@ -2,17 +2,21 @@ from .models import UserProfile
 from django.contrib.auth.models import User
 from django import forms
 
+
 # jai pas utiliser le user  update form mais je dois le mettre dans les updates views note pour Elo pour l update
 class UserUpdateForm(forms.ModelForm):
-    email = forms.EmailField()
+    email = forms.EmailField(required=False)
+
     class Meta:
         model = User
-        fields = ['username','email']
+        fields = ['username', 'email']
+
 
 class ProfileUpdateForm(forms.ModelForm):
     class Meta:
         model = UserProfile
         fields = ['country']
+
 
 class AvatarUpdateForm(forms.ModelForm):
     avatar = forms.ImageField(
