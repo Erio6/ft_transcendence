0972f39bf20d30498134ac60f72388dfb4ae8a7e
elo <elodie.hamm@gmail.com>
2025-02-05 16:30:00 +0100

    fixing blockchain

--- src/backend/game/online_room.py
+++ src/backend/game/online_room.py
@@ -1,5 +1,6 @@
 import asyncio
 import json
+import threading
 
 from asgiref.sync import sync_to_async
 from django.shortcuts import redirect
@@ -82,19 +83,7 @@ class OnlineRoom(Room):
             await consumer.send(json.dumps({'type': 'redirect', 'url': "/"}))
             await consumer.close()
         if not self.is_tournament:
-            asyncio.create_task(self.blockchain_recording_task(game.id))
-
-    async def blockchain_recording_task(self,game_id):
-        tx_hash = await blockchain_score_storage(game_id)
-        game = await sync_to_async(Game.objects.get)(pk=game_id)
-        if tx_hash:
-            print(f"Game recorded on blockchain with tx_hash: {tx_hash}")
-            game.tx_hash = tx_hash
-            game.is_recorded_on_blockchain = True
-        else:
-            print("Failed to record game on blockchain.")
-
-        await sync_to_async(game.save)(force_update=True)
+            threading.Thread(target=self.blockchain_recording_task,daemon=True, args=(self.id,game.id)).start()
 
     async def force_end(self, looser_left=True):
         winner = self.right_paddle if looser_left else self.left_paddle
